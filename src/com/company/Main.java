b26fdad0ce2b5ff0bdf00ee9afecb5f802ee18d8
Roman <romanmaistruk@gamil.com>
2018-08-28 20:15:17 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,29 @@
+package com.company;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    static Random randm = new Random();
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int  scanNum = 0;
+        int mynum = randm.nextInt(100);
+        System.out.println(mynum);
+
+        for (int i = 0; i < 10; i++) {
+             scanNum = scan.nextInt();
+        }
+        if (scanNum > mynum) {
+            System.out.println("Моё число больше!");
+        } else if (scanNum < mynum) {
+            System.out.println("Моё число меньше!");
+        } else if (scanNum == mynum) {
+            System.out.println("Оба числа равны!");
+        }
+
+
+    }
+}
