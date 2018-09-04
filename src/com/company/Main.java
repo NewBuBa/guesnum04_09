62f46fccd45726ec92a57939afc083c03fabaa53
java_tsi08 <artjom.cerkasskijs@gmail.com>
2018-09-04 19:06:24 +0300

    19:06_09_2018

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,28 +8,32 @@ public class Main {
     static Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) {
-
+        String answer;
         int scanNum = 0;
-        int mynum = randm.nextInt(100);
-        System.out.println(mynum);
-        boolean userLost = true;
-        for (int i = 0; i < 10; i++) {
-            scanNum = scan.nextInt();
+        do {
+            int mynum = randm.nextInt(100);
+            System.out.println(mynum);
+            boolean userLost = true;
+            for (int i = 0; i < 10; i++) {
+                scanNum = scan.nextInt();
 
-            if (scanNum > mynum) {
-                System.out.println("Моё число больше!");
-            } else if (scanNum < mynum) {
-                System.out.println("Моё число меньше!");
-            } else if (scanNum == mynum) {
-                System.out.println("Оба числа равны!");
-                break;
-            }
-                if (userLost == true){
-                    System.out.println("test 2");
+                if (scanNum > mynum) {
+                    System.out.println("Моё число больше!");
+                } else if (scanNum < mynum) {
+                    System.out.println("Моё число меньше!");
+                } else if (scanNum == mynum) {
+                    System.out.println("Оба числа равны!");
+                    break;
                 }
-
             }
-
-        }
+//            if (userLost == true) {
+//                System.out.println("test 2");
+//            }
+            System.out.println("YEs / NO");
+            answer = scan.next();
+        } while (answer.equals("Y"));
+        System.out.println("Bay");
     }
+}
+
 
